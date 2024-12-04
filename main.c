187986e34dc03004e4c46e3154493310c6453ed0
Quynhtrox <nnqnq05022003@gmail.com>
2024-12-04 13:38:34 +0700

    4th time

--- main.c
+++ main.c
@@ -84,11 +84,13 @@ void add_data(Shared_data *shared, Sensor_data data) {
 }
 
 //Get data from buffer
-Sensor_data get_data(Shared_data *shared, Sensor_data data) {
+Sensor_data get_data(Shared_data *shared) {
+    Sensor_data data = {0};
     pthread_mutex_lock(&data_lock);
         data = shared->buffer[shared->head];
         shared->head = (shared->head + 1) % MAX_BUFFER_SIZE;
     pthread_mutex_unlock(&data_lock);
+    return data;
 }
 
 //Connection manager thread
@@ -135,8 +137,8 @@ static void *thr_connection(void *args) {
     log_events(log_message);
 
     } else {
-    Sensor_data data;
-    get_data(shared, data);
+    Sensor_data data = get_data(shared);
+
     //Ghi log closed
     char log_message[MAX_BUFFER_SIZE];
     snprintf(log_message, sizeof(log_message), "The sensor node with %d has closed the connection\n", data.SensorNodeID);
@@ -149,7 +151,23 @@ static void *thr_connection(void *args) {
 
 //Data manager thread
 static void *thr_data(void *args) {
+    Shared_data *shared = (Shared_data *)args;
 
+    while (1)
+    {
+        Sensor_data data = get_data(shared);
+        if (data.SensorNodeID !=0 ) {
+            char log_message[MAX_BUFFER_SIZE];
+            if (data.temperature > 30.0) {
+                snprintf(log_message, sizeof(log_message), "The sensor node with %d reports it’s too hot (running avg temperature = %.1f)\n", data.SensorNodeID, data.temperature);
+                log_events(log_message);
+            } else if (data.temperature < 15) {
+                snprintf(log_message, sizeof(log_message), "The sensor node with %d reports it’s too cold (running avg temperature = %.1f)\n", data.SensorNodeID, data.temperature);
+                log_events(log_message);
+            }
+        }
+    }
+    
 }
 
 //Storage manager thread
